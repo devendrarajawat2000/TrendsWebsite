83efe43b581d385b633697a76df59341106c4936
devendrarajawat2000 <devendrarajawat2000@gmail.com>
2024-05-29 17:41:25 +0530

    new code

--- Trends/index.js
+++ Trends/index.js
@@ -1,31 +1,24 @@
 let buttons = document.querySelectorAll(".btn");
-
+let cartCount = 0 ;
 buttons.forEach(button => {
   button.addEventListener("click", (e) => {
     if (e.target.innerHTML === "Add to Cart") {
       e.target.innerHTML = "Remove from Cart";
+      cartCount++;
     } else {
       e.target.innerHTML = "Add to Cart";
+      cartCount--;
     }
+    document.getElementById("cart-count").innerText = cartCount;
   });
 });
 
 let signUpButton = document.querySelector(".sign");
 signUpButton.addEventListener("click", () => {
   let emailInput = document.querySelector(".inp input").value;
-  if (validateEmail(emailInput)) {
+  if (emailInput.includes("@") && emailInput.includes(".")) {
     alert("Thank you for signing up!");
   } else {
     alert("Please enter a valid email address.");
   }
 });
-function validateEmail(email) {
-  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return re.test(email);
-}
-
-
-
-
-
-
